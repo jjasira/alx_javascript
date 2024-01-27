372af73095d4313dee6196872ef1ca3bd14c3ed7
James Asira <jjasira2016@gmail.com>
2024-01-27 12:37:32 +0300

    revision

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,15 @@
-const var1 = "C is fun";
-const var2 = "Python is cool";
-const var3 = "Javascript is amazing";
-const myArray = [var1, var2, var3];
+#!/usr/bin/node
+// const var1 = "C is fun";
+// const var2 = "Python is cool";
+// const var3 = "Javascript is amazing";
+// const myArray = [var1, var2, var3];
 
-for (let i = 0; i < myArray.length; i++) {
-  console.log(myArray[i]);
+// for (let i = 0; i < myArray.length; i++) {
+//   console.log(myArray[i]);
+// }
+
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (const language of languages) {
+  console.log(language);
 }
