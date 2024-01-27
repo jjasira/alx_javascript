2b59f56c511d77857ef6dd538fc68798e0f94cdb
James Asira <jjasira2016@gmail.com>
2024-01-27 11:24:28 +0300

    revision

---
+++ javascript-warm_up/103-object_fct.js
@@ -0,0 +1,19 @@
+const myObject = {
+  type: "object",
+  value: 12,
+};
+
+console.log(myObject);
+
+myObject.incr = function () {
+  this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
