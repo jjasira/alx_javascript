1a7bce01144911ef3c6e523c612bb9d6393bcace
James Asira <jjasira2016@gmail.com>
2024-01-27 12:28:13 +0300

    revision

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -31,17 +31,5 @@ class Square extends Rectangle {
   constructor(size) {
     super(size, size);
   }
-
-  charPrint(c) {
-    if (c !== undefined) {
-      for (let i = 0; i < this.size; i++) {
-        console.log("c".repeat(this.size));
-      }
-    } else {
-      for (let i = 0; i < this.size; i++) {
-        console.log("X".repeat(this.size));
-      }
-    }
-  }
 }
 module.exports = Square;
--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,17 @@
-const Square = require("./5-square");
+const Squarebase = require("./5-square");
+
+class Square extends Squarebase {
+  charPrint(c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.size; i++) {
+        console.log("c".repeat(this.size));
+      }
+    } else {
+      for (let i = 0; i < this.size; i++) {
+        console.log("X".repeat(this.size));
+      }
+    }
+  }
+}
+
+module.exports = Square;
