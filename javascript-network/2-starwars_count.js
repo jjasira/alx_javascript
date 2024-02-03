1c5a9a569285f6fa8a3e5c567e5127bd6ca5b0ea
James Asira <jjasira2016@gmail.com>
2024-02-03 10:22:11 +0300

    filtering results

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,28 @@
+const request = require("request");
+
+const countMoviesWithWedgeAntilles = (apiUrl) => {
+  request(apiUrl, (error, response, body) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+    } else if (response.statusCode !== 200) {
+      console.error(`Error: Unexpected status code ${response.statusCode}`);
+    } else {
+      try {
+        const filmsData = JSON.parse(body);
+        const moviesWithWedgeAntilles = filmsData.results.filter((film) =>
+          film.characters.includes(
+            "https://swapi-api.alx-tools.com/api/people/18/"
+          )
+        );
+        console.log(
+          `Number of movies with Wedge Antilles: ${moviesWithWedgeAntilles.length}`
+        );
+      } catch (parseError) {
+        console.error(`Error parsing JSON: ${parseError.message}`);
+      }
+    }
+  });
+};
+
+const apiUrl = process.argv[2];
+countMoviesWithWedgeAntilles(apiUrl);
