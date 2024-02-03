1d913b7f5574c22f597b91179e689c2c9625e77e
James Asira <jjasira2016@gmail.com>
2024-02-03 13:30:03 +0300

    call stack

---
+++ Javascript_advanced/7-timeout.js
@@ -0,0 +1,9 @@
+console.log("Start of the execution queue");
+setTimeout(() => {
+  console.log("Final code block to be executed");
+}, 0);
+for (let i = 0; i < 100; i++) {
+  console.log(i + 1);
+}
+
+console.log("End of the loop printing");
